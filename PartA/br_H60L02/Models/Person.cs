fdb6e375911392c1c4e7f6d8cd0f2509cbd746c5
Rouleau <2033645@cegep-heritage.qc.ca>
2024-08-26 15:36:48 -0400

    H60 Lab 2

---
+++ PartA/br_H60L02/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace br_H60L02.Models;
+
+public partial class Person
+{
+    public decimal PersonId { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public decimal CityId { get; set; }
+
+    public virtual City City { get; set; } = null!;
+}
---
+++ PartA/br_H60L02/Models/Pet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace br_H60L02.Models;
+
+public partial class Pet
+{
+    public decimal PetId { get; set; }
+
+    public string? PetName { get; set; }
+}
